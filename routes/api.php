34b0bc883cb74eab51549bace635305562231da9
Tonymans33 <tonymansour66@yahoo.com>
2020-12-17 20:21:30 +0200

    Adding auth api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\Auth\DataController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//auth routes group
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('register', [UserController::class, 'register']);
+    Route::post('login', [UserController::class, 'authenticate']);
+    Route::get('logout', [UserController::class, 'logout']);
+    Route::get('open',[DataController::class, 'open']);
+});
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
