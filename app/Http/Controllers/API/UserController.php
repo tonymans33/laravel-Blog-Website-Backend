5adf26e15cacb51b2ac0865690218c7b1b97be3f
Tonymans33 <tonymansour66@yahoo.com>
2020-12-17 19:35:47 +0200

    Adding Jwt authentication functions to user model

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
